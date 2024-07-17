bf8fba83d3463b873a7b5b0e263c2342d2d6efc6
Dustpie <dustin.pyka@icloud.com>
2024-07-17 12:26:03 +0200

    Routing implementation

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,32 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { interval, Subscription } from 'rxjs';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private snackbar = inject(ToastrService);
   stateLog: any = {};
-  private stateUpdateSubscription!: Subscription;
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
+        this.snackbar.success('Login was succesful');
+        console.log(this.model);
       },
-      error: (error) => console.log(error),
+      error: (error) => this.snackbar.error(error.error),
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
